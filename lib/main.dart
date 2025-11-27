69c0eb3f3a7402c4ef771426bb681ca8e0635b2b
Khawarizmi14 <khawaydin@gmail.com>
2025-11-27 12:15:15 +0700

    feat: login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:dribbl_id/main/screens/login.dart';
 import 'package:flutter/material.dart';
-import 'package:dribbl_id/main/screens/menu.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,29 +12,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        // Set the background to black
-        scaffoldBackgroundColor: Colors.black,
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        theme: ThemeData(
+          // Set the background to black
+          scaffoldBackgroundColor: Colors.black,
 
-        // Define the color scheme with Blue as the seed
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.dark, // Ensures text is white on black
-          primary: Colors.blue,
-          secondary: Colors.lightBlueAccent,
-          surface: Colors.black, // Background for cards/sheets
+          // Define the color scheme with Blue as the seed
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+            brightness: Brightness.dark, // Ensures text is white on black
+            primary: Colors.blue,
+            secondary: Colors.lightBlueAccent,
+            surface: Colors.black, // Background for cards/sheets
+          ),
+
+          // Ensure app bar and other elements match the dark theme
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+          ),
+          useMaterial3: true,
         ),
 
-        // Ensure app bar and other elements match the dark theme
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white,
-        ),
-        useMaterial3: true,
+        home: const LoginPage(),
       ),
-
-      home: MainPage(),
     );
   }
 }
