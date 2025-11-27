1956d4108155df0d245aecb00bc8b38935513e1f
Khawarizmi14 <khawaydin@gmail.com>
2025-11-27 11:37:12 +0700

    rename menu.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dribbl_id/main/screens/main.dart';
+import 'package:dribbl_id/main/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
