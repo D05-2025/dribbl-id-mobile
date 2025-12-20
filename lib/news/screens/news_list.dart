66aca6e0b59e4976827933df8356fcebd3192f3d
rayayayya <raynabalq.s@gmail.com>
2025-12-21 05:34:17 +0700

    mau pindahin ke prod

--- lib/news/screens/news_list.dart
+++ lib/news/screens/news_list.dart
@@ -29,7 +29,7 @@ class _NewsEntryListPageState extends State<NewsEntryListPage> {
   ];
 
   Future<List<News>> fetchNews(CookieRequest request) async {
-    final response = await request.get('https://febrian-abimanyu-dribbl-id.pbp.cs.ui.ac.id/news/json/');
+    final response = await request.get('http:localhost:8000/news/json/');
     List<News> listNews = response.map<News>((d) => News.fromJson(d)).toList();
 
     if (_selectedCategory != 'All') {
